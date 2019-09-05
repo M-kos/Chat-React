c15c498159ef011dd0d981bf470e870d6693f821
M-kos <koc.melnikov.1985@gmail.com>
2019-09-06 00:21:30 +0700

    configure react-router and redirect

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,47 @@
 import React, {Component} from 'react';
 import openSocket from 'socket.io-client';
+import {Redirect} from 'react-router-dom';
 
 import './App.css';
 
 export default class App extends Component {
 
+  state = {
+    idRoom: ''
+  }
+
   componentDidMount() {
     const socket = openSocket('http://localhost:8080/');
 
-    socket.on('connect', function () {
+    socket.on('connect', () => {
       console.log('connected to socket');
     });
-    socket.on('disconnect', function () {
+    socket.on('onConnection', (id) => {
+      console.log('ID: ', id);
+      this.setState({
+        idRoom: id.id
+      })
+    })
+    socket.on('disconnect', () => {
       console.log('disconnected from socket');
     });
   }
 
 
 
+
   render() {
+    const {idRoom} = this.state;
+    console.log("Location");
+    console.log(this.props.location);
+    console.log("Match");
+    console.log(this.props.match);
+    return (
+      <div>
+        <Redirect to={`/${idRoom}`} />
+        <h1>Hello</h1>
+      </div>
+    );
   };
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={App}/>
+            <Route exact path='/:id' component={App}/>
+        </Switch>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 
 serviceWorker.unregister();
